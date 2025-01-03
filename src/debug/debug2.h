dd290a89f041062132651ea45f2084672bd850c8
fredrik-hjarner <35084243+fredrik-hjarner@users.noreply.github.com>
2025-01-03 03:20:37 +0100

    a pipe worked super well!!!!!!! It was not too slow or anything, faster than writing and reading a file at least.

--- src/debug/debug2.h
+++ src/debug/debug2.h
@@ -84,7 +84,7 @@ static std::set<std::string> uniqueAddresses;
 // << EMS window 4 >> ec00-efff
 
 static bool IsGraphics565A(uint16_t segment) {
-	return segment == 0x565A;
+	return segment == 0x565A; // This code includes the loop waiting for vertical trace.
 }
 static bool IsGraphics5677(uint16_t segment) {
 	return segment == 0x5677;
@@ -114,82 +114,153 @@ static bool IsMotherboardBIOS(uint16_t segment) {
 	return (segment >= 0xF000 && segment <= 0xFFFF);
 }
 
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+
+class IPCPipe {
+    int fd;
+    const char* pipePath = "/tmp/dosbox_debug_pipe";
+    bool isOpen;
+    int writeCounter = 0;
+    const int CHECK_INTERVAL = 200;
+
+public:
+    IPCPipe() : fd(-1), isOpen(false) {
+        // Create the named pipe if it doesn't exist
+        if (mkfifo(pipePath, 0666) == -1 && errno != EEXIST) {
+            std::cerr << "\n\n*** ERROR: Failed to create debug pipe: " << strerror(errno) 
+                     << " (errno: " << errno << ") ***\n\n" << std::endl;
+        }
+    }
+
+    bool openPipe() {
+        if (!isOpen) {
+            // Open pipe for writing, non-blocking
+            // fd = open(pipePath, O_WRONLY | O_NONBLOCK);
+            fd = ::open(pipePath, O_WRONLY);
+            if (fd == -1) {
+                std::cerr << "\n\n*** ERROR: Failed to open debug pipe: " << strerror(errno)
+                         << " (errno: " << errno << ") ***\n"
+                         << "*** Pipe path: " << pipePath << " ***\n\n" << std::endl;
+                return false;
+            }
+            isOpen = (fd != -1);
+        }
+        return isOpen;
+    }
+
+    void write(const std::string& data) {
+        if (isOpen) {
+            if (::write(fd, data.c_str(), data.length()) == -1) {
+                std::cerr << "\n\n*** ERROR: Failed to write to debug pipe: " << strerror(errno)
+                         << " (errno: " << errno << ") ***\n"
+                         << "*** Pipe path: " << pipePath << " ***\n\n" << std::endl;
+            }
+
+            // Periodically check buffer size
+            if (++writeCounter >= CHECK_INTERVAL) {
+                int bytesAvailable;
+                if (ioctl(fd, FIONREAD, &bytesAvailable) != -1) {
+					// TODO: Or I can show something "graphically" like a progress bar =========== that shrinks and grows.
+                    std::cerr << "\n*** DEBUG PIPE STATUS ***\n"
+                             << "Bytes in buffer: " << bytesAvailable 
+                             << " bytes (" << (bytesAvailable / 1024.0) << " KB)" << std::endl;
+                }
+                writeCounter = 0;  // Reset counter after check
+            }
+        }
+    }
+
+    ~IPCPipe() {
+        if (isOpen) {
+            ::close(fd);
+        }
+    }
+};
+
+static IPCPipe debugPipe;
+
 // TODO: Refactor this is getting messy. I could have more small helper functions.
 static void LogInstruction2(uint16_t segValue, uint32_t eipValue, ofstream& out) {
-	// TODO: This is just a hack.
-	if (!cpuLogFile.is_open()) {
-		cpuLogFile.open("LOGCPU.TXT");
-	}
+    // Remove file handling code
+    // if (!cpuLogFile.is_open()) {
+    //     cpuLogFile.open("LOGCPU.TXT");
+    // }
 
-	static std::stringstream buffer;
-	static int bufferCount = 0;
-	const int BUFFER_FLUSH_SIZE = 20;
+    static std::stringstream buffer;
+    static int bufferCount = 0;
+    const int BUFFER_FLUSH_SIZE = 20;
 
-	// Format the address
-	std::stringstream addressStream;
-	addressStream << std::hex << std::uppercase << std::setfill('0') 
-				 << std::setw(4) << SegValue(cs) << ":"
-				 << std::setw(4) << reg_eip;
-	std::string address = addressStream.str();
-	
-	// Only add to buffer if this is a new address
-	if (uniqueAddresses.insert(address).second) {
-		// Get the physical address and disassemble
-		PhysPt start = (PhysPt)GetAddress(segValue, eipValue);
-		char dline[200];
-		Bitu size = DasmI386(dline, start, reg_eip, cpu.code.big);
+    // Format the address
+    std::stringstream addressStream;
+    addressStream << std::hex << std::uppercase << std::setfill('0') 
+                 << std::setw(4) << SegValue(cs) << ":"
+                 << std::setw(4) << reg_eip;
+    std::string address = addressStream.str();
+    
+    // Only add to buffer if this is a new address
+    if (uniqueAddresses.insert(address).second || true) { // TODO: The true is just a hack. I wanted to test to output it all.
+        // Get the physical address and disassemble
+        PhysPt start = (PhysPt)GetAddress(segValue, eipValue);
+        char dline[200];
+        Bitu size = DasmI386(dline, start, reg_eip, cpu.code.big);
 
-		// Get the instruction bytes
-		std::stringstream bytesStream;
-		for (Bitu i=0; i<size; i++) {
-			uint8_t value;
-			if (mem_readb_checked((PhysPt)(start+i), &value)) {
-				bytesStream << "?? ";
-			} else {
-				bytesStream << std::hex << std::uppercase << std::setfill('0') 
-						<< std::setw(2) << static_cast<int>(value) << " ";
-			}
-		}
-		
-		buffer << address << "    "  // 4 spaces after address
-			   << bytesStream.str() 
-			   << std::setfill(' ') << std::setw(30 - bytesStream.str().length()) << " "  // padding between opcodes and asm
-			   << std::setw(35) << std::left << dline;  // left-aligned assembly with 30 char width
+        // Get the instruction bytes
+        std::stringstream bytesStream;
+        for (Bitu i=0; i<size; i++) {
+            uint8_t value;
+            if (mem_readb_checked((PhysPt)(start+i), &value)) {
+                bytesStream << "?? ";
+            } else {
+                bytesStream << std::hex << std::uppercase << std::setfill('0') 
+                         << std::setw(2) << static_cast<int>(value) << " ";
+            }
+        }
+        
+        buffer << address << "    "  // 4 spaces after address
+               << bytesStream.str() 
+               << std::setfill(' ') << std::setw(30 - bytesStream.str().length()) << " "  // padding between opcodes and asm
+               << std::setw(35) << std::left << dline;  // left-aligned assembly with 30 char width
 
-		// Add BIOS indicator if in BIOS range
-		if (IsMotherboardBIOS(SegValue(cs))) {
-			buffer << std::setw(25) << "<< Motherboard BIOS >>";
-		} else if (IsVideoBIOS(SegValue(cs))) {
-			buffer << std::setw(25) << "<< Video BIOS >>";
-		} else if (IsEmsWindow1(SegValue(cs))) {
-			buffer << std::setw(25) << "<< EMS window 1 >>";
-		} else if (IsEmsWindow2(SegValue(cs))) {
-			buffer << std::setw(25) << "<< EMS window 2 >>";
-		} else if (IsEmsWindow3(SegValue(cs))) {
-			buffer << std::setw(25) << "<< EMS window 3 >>";
-		} else if (IsEmsWindow4(SegValue(cs))) {
-			buffer << std::setw(25) << "<< EMS window 4 >>";
-		} else if (IsGraphics565A(SegValue(cs))) {
-			buffer << std::setw(25) << "<< Graphics1 cant find in EXE >>";
-		} else if (IsGraphics5677(SegValue(cs))) {
-			buffer << std::setw(25) << "<< Graphics2 cant find in EXE >>";
-		} else if (IsGraphics5FBE(SegValue(cs))) {
-			buffer << std::setw(25) << "<< Graphics3 cant find in EXE >>";
-		}
-		
-		buffer << "\n";
-	}
-	bufferCount++;
+        // Add BIOS indicator if in BIOS range
+        if (IsMotherboardBIOS(SegValue(cs))) {
+            buffer << std::setw(25) << "<< Motherboard BIOS >>";
+        } else if (IsVideoBIOS(SegValue(cs))) {
+            buffer << std::setw(25) << "<< Video BIOS >>";
+        } else if (IsEmsWindow1(SegValue(cs))) {
+            buffer << std::setw(25) << "<< EMS window 1 >>";
+        } else if (IsEmsWindow2(SegValue(cs))) {
+            buffer << std::setw(25) << "<< EMS window 2 >>";
+        } else if (IsEmsWindow3(SegValue(cs))) {
+            buffer << std::setw(25) << "<< EMS window 3 >>";
+        } else if (IsEmsWindow4(SegValue(cs))) {
+            buffer << std::setw(25) << "<< EMS window 4 >>";
+        } else if (IsGraphics565A(SegValue(cs))) {
+            buffer << std::setw(25) << "<< Graphics1 cant find in EXE >>";
+        } else if (IsGraphics5677(SegValue(cs))) {
+            buffer << std::setw(25) << "<< Graphics2 cant find in EXE >>";
+        } else if (IsGraphics5FBE(SegValue(cs))) {
+            buffer << std::setw(25) << "<< Graphics3 cant find in EXE >>";
+        }
+        
+        buffer << "\n";
+    }
+    bufferCount++;
 
-	std::string bufferAsString = buffer.str();
-	
-	if (bufferCount >= BUFFER_FLUSH_SIZE && bufferAsString.length() > 0) {
-		out << bufferAsString;
-		
-		buffer.str("");
-		buffer.clear();
-		bufferCount = 0;
-	}
+    std::string bufferAsString = buffer.str();
+    
+    if (bufferCount >= BUFFER_FLUSH_SIZE && bufferAsString.length() > 0) {
+        // Instead of writing to file, write to pipe
+        if (debugPipe.openPipe()) {
+            debugPipe.write(bufferAsString);
+        }
+        
+        buffer.str("");
+        buffer.clear();
+        bufferCount = 0;
+    }
 }
 
 /*
@@ -199,12 +270,19 @@ TODO:
 - Add many more items to my "memory map". I can add functions that I name for example.
   - I can even for certain code or functions only show like that it was called and exited and skips the rest if it's not interesting.
     - I can have som kind of toggle in my map to show or hide the code.
+  - vsync loop should probably be totally hidden to not cause spam.
+  - certain functions are the "accumulated" for example say some graphics function that runs 100 times in a row, then I just show 1 line.
 - I probably should add some or all registers because then I could see for example which places in memory was fetched and updated.
 - I have an idea of using named pipe.
   - dosbox-x just spits out every single instruction to a named pipe,
   - then js listens to it and it can either display it in terminal but also take commands at the same time.
     - js can have command to listen or to discard or maybe to do memory dumps too (but I dont quite know how that would work).
 	  - regarding memory dumps they should get unique names, not like now in dosvox-x when LOGCPU.TXT gets overwritten.
+- Save and reload state would be very valuable when debugging, try something then rewind change a variable or try something else
+  then combine it with mem or instruction diffing to see what the difference was!!!
+  Yes, dosbox-x has save state and load state!
+- I could probably figure out a way to trigger a breakpoint x steps before the breakpoint
+  (if the instruction is visible under in code window).
 
 
 
