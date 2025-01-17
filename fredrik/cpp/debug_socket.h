8fe69f47019f0a0a1c3f5089e54ac43c80577900
fredrik-hjarner <35084243+fredrik-hjarner@users.noreply.github.com>
2025-01-17 17:12:21 +0100

    WIP: moved socket to its own file, I didnt check if it still works though.........

---
+++ fredrik/cpp/debug_socket.h
@@ -0,0 +1,71 @@
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <sys/un.h>
+
+// TODO: autoDisassemblerMode does not need UnixSocketSender at all.
+class UnixSocketSender {
+private:
+    int sock_fd;
+    struct sockaddr_un addr;
+    bool is_connected;
+    
+public:
+    UnixSocketSender() : sock_fd(-1), is_connected(false) {
+        // Create socket
+        sock_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+        if (sock_fd == -1) {
+            // Handle socket creation error
+            std::cerr << "\n\n*** ERROR: Failed to create debug socket: " << strerror(errno) << " (errno: " << errno << ") ***\n\n" << std::endl;
+            return;
+        }
+        
+        // Setup address structure
+        memset(&addr, 0, sizeof(addr));
+        addr.sun_family = AF_UNIX;
+        strncpy(addr.sun_path, "/tmp/debug_socket", sizeof(addr.sun_path) - 1);
+
+        this->connect(); // connect immediately in constructor.
+    }
+    
+    bool connect() {
+        if (is_connected) return true;
+        if (sock_fd == -1) {
+            std::cerr << "\n\n*** ERROR: connect: Failed to create debug socket: " << strerror(errno) << " (errno: " << errno << ") ***\n\n" << std::endl;
+            return false;
+        }
+        
+        is_connected = (::connect(sock_fd, (struct sockaddr*)&addr, sizeof(addr)) == 0);
+        if (!is_connected) {
+            std::cerr << "\n\n*** ERROR: connect: Failed to connect to debug socket: " << strerror(errno) << " (errno: " << errno << ") ***\n\n" << std::endl;
+        }
+        return is_connected;
+    }
+    
+    bool write(const std::string& message) {
+        if (!is_connected) {
+            std::cerr << "\n\n*** ERROR: write: Failed to connect to debug socket: " << strerror(errno) << " (errno: " << errno << ") ***\n\n" << std::endl;
+            return false;
+        }
+
+        // TODO: This might be faster but I don't know.
+        // Just send the message directly
+        std::size_t length = ::write(sock_fd, message.c_str(), message.length());
+        if (length != message.length()) {
+            std::cerr << "\n\n*** ERROR: write: Failed to send message: " << strerror(errno) << " (errno: " << errno << ") ***\n\n" << std::endl;
+            is_connected = false;
+            return false;
+        }
+        return true;
+    }
+    
+    ~UnixSocketSender() {
+        if (sock_fd != -1) {
+            close(sock_fd);
+        }
+    }
+};
+
+// TODO: autoDisassemblerMode does not need UnixSocketSender at all.
+static UnixSocketSender debugSocket;
--- fredrik/cpp/main.h
+++ fredrik/cpp/main.h
@@ -1,9 +1,10 @@
 #include "globals_and_consts.h"
+#include "debug_socket.h"
 #include "overlays2.h"
 #include "overlays.h"
-#include "address.h"
-#include "address_range.h"
-#include "address_ranges.h"
+// #include "address.h"
+// #include "address_range.h"
+// #include "address_ranges.h"
 #include "get_cpu_instruction_line_string.h"
 
 // No. Dont add any more includes!!!
@@ -70,75 +71,6 @@ static bool SkipSendingInstruction(uint16_t segment, uint32_t offset) {
     return false;
 }
 
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <sys/ioctl.h>
-#include <sys/un.h>
-
-// TODO: autoDisassemblerMode does not need UnixSocketSender at all.
-class UnixSocketSender {
-private:
-    int sock_fd;
-    struct sockaddr_un addr;
-    bool is_connected;
-    
-public:
-    UnixSocketSender() : sock_fd(-1), is_connected(false) {
-        // Create socket
-        sock_fd = socket(AF_UNIX, SOCK_STREAM, 0);
-        if (sock_fd == -1) {
-            // Handle socket creation error
-            std::cerr << "\n\n*** ERROR: Failed to create debug socket: " << strerror(errno) << " (errno: " << errno << ") ***\n\n" << std::endl;
-            return;
-        }
-        
-        // Setup address structure
-        memset(&addr, 0, sizeof(addr));
-        addr.sun_family = AF_UNIX;
-        strncpy(addr.sun_path, "/tmp/debug_socket", sizeof(addr.sun_path) - 1);
-
-        this->connect(); // connect immediately in constructor.
-    }
-    
-    bool connect() {
-        if (is_connected) return true;
-        if (sock_fd == -1) {
-            std::cerr << "\n\n*** ERROR: connect: Failed to create debug socket: " << strerror(errno) << " (errno: " << errno << ") ***\n\n" << std::endl;
-            return false;
-        }
-        
-        is_connected = (::connect(sock_fd, (struct sockaddr*)&addr, sizeof(addr)) == 0);
-        if (!is_connected) {
-            std::cerr << "\n\n*** ERROR: connect: Failed to connect to debug socket: " << strerror(errno) << " (errno: " << errno << ") ***\n\n" << std::endl;
-        }
-        return is_connected;
-    }
-    
-    bool write(const std::string& message) {
-        if (!is_connected) {
-            std::cerr << "\n\n*** ERROR: write: Failed to connect to debug socket: " << strerror(errno) << " (errno: " << errno << ") ***\n\n" << std::endl;
-            return false;
-        }
-
-        // TODO: This might be faster but I don't know.
-        // Just send the message directly
-        std::size_t length = ::write(sock_fd, message.c_str(), message.length());
-        if (length != message.length()) {
-            std::cerr << "\n\n*** ERROR: write: Failed to send message: " << strerror(errno) << " (errno: " << errno << ") ***\n\n" << std::endl;
-            is_connected = false;
-            return false;
-        }
-        return true;
-    }
-    
-    ~UnixSocketSender() {
-        if (sock_fd != -1) {
-            close(sock_fd);
-        }
-    }
-};
-
 std::string GetLabelForAddress(uint16_t segValue, uint32_t eipValue) {
     if (IsVsync(segValue, eipValue)) {
         return "WAIT_FOR_VSYNC";
@@ -163,9 +95,6 @@ std::string GetLabelForAddress(uint16_t segValue, uint32_t eipValue) {
     }
     return "";
 }
-
-// TODO: autoDisassemblerMode does not need UnixSocketSender at all.
-static UnixSocketSender debugSocket;
 
 // When this is 1 it's very very slow, so I added this buffer to speed up things.
 // Things are not outputted immediately, but are collected in buffer for a short while.
